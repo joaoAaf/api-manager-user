312a80d6ddf64007c273f0c6a87fb27f04f6b7cb
joaoAaf <joao.anderson.af@gmail.com>
2024-03-16 14:18:48 -0300

    refatorando SecurityFilterChain

--- src/main/java/apiManagerUser/config/WebConfig.java
+++ src/main/java/apiManagerUser/config/WebConfig.java
@@ -21,37 +21,35 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 @EnableWebSecurity
 public class WebConfig implements WebMvcConfigurer {
-	 
-	    @Autowired
-	    private FilterToken filter;
-	
-		public void addCorsMappings(CorsRegistry registry) {
-	        registry.addMapping("/**");
-	    }
-	    
-	    @Bean
-	    public SecurityFilterChain sfc (HttpSecurity httpSec) throws Exception {
-	    	return httpSec.csrf().disable()
-	    			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-	    			.and().authorizeHttpRequests()
-	    			.requestMatchers(HttpMethod.POST,"/login")
-	    			.permitAll()
-	    			.anyRequest().authenticated()
-	    			.and().addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
-	    			.build();
-	    }
-	    
-	    
-	    @Bean
-	    public AuthenticationManager authM (AuthenticationConfiguration authConfig) throws Exception {
-	    	return authConfig.getAuthenticationManager();
-	    	
-	    }
-	    
-	    @Bean
-	    public PasswordEncoder passEncod () {
-	    	return new BCryptPasswordEncoder();
-	    }
-	    
-	    
+
+	@Autowired
+	private FilterToken filter;
+
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**");
+	}
+
+	@Bean
+	public SecurityFilterChain sfc(HttpSecurity httpSec) throws Exception {
+		return httpSec.csrf(csrf -> csrf.disable())
+				.sessionManagement(management -> management.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+				.authorizeHttpRequests(requests -> requests
+						.requestMatchers(HttpMethod.POST, "/login")
+						.permitAll()
+						.anyRequest().authenticated())
+				.addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+				.build();
+	}
+
+	@Bean
+	public AuthenticationManager authM(AuthenticationConfiguration authConfig) throws Exception {
+		return authConfig.getAuthenticationManager();
+
+	}
+
+	@Bean
+	public PasswordEncoder passEncod() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
